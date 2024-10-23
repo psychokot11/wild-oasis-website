91c4db944a3bb7e7d1426e70624e3cffc9af794e
Zofia Stawarska <zstawarska@future-processing.com>
2024-10-23 17:46:04 +0200

    project structure

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -8,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-colors-primary-950 text-colors-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
