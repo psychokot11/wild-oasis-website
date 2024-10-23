276817cb0b6ea75ca6e8c46b34dd6a05045ba434
Zofia Stawarska <zstawarska@future-processing.com>
2024-10-23 18:58:49 +0200

    img optimization

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      <Image src={logo} alt="The Wild Oasis logo" height="60" width="60" quality={100}/>
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
